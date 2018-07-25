23534fb615631f7ce0fc17116f6059f9dd443cba
natsutan <YRW03704@nifty.com>
2018-07-26 01:56:59 +0900

    work

--- abnormaly_detection/isolate_forest/isolate_forest2.py
+++ abnormaly_detection/isolate_forest/isolate_forest2.py
@@ -15,37 +15,46 @@ heights = [102, 102, 103, 103, 103, 104, 104, 104, 105, 105, 105, 105, 105, 105,
            110, 110, 110, 110, 110, 110, 110, 111, 111, 111, 111, 111, 111, 111, 111, 111, 111, 111, 111, 111, 112,
            112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 113, 113, 113, 113, 113, 113, 113, 113, 113, 113,
            113, 113, 114, 114, 114, 114, 114, 114, 114, 114, 114, 114, 115, 115, 115, 115, 115, 115, 116, 116, 116,
-           116, 117, 118, ]
+           116, 117, 118]
+
+weights = [ 15, 16, 15, 16, 17, 15, 16, 17, 16, 16, 17, 17, 17, 18, 16, 16, 16, 17, 17, 17, 18, 18, 19, 16, 17, 17,
+            17, 17, 18, 18, 18, 18, 19, 16, 17, 17, 17, 17, 18, 18, 18, 18, 19 , 19 , 19 , 20, 16, 17, 17, 18, 18,
+            18, 18, 19, 19, 19, 20, 17, 17, 18, 18, 18, 18, 19, 19, 19, 19, 20, 20, 21, 17, 17, 18, 18, 18, 19, 19,
+            19, 19, 20, 20, 20, 21, 17, 18, 18, 18, 19, 19, 19, 19, 20, 20, 20, 21, 18, 18, 18, 19, 19, 19, 20, 20,
+            20, 21, 21, 22, 18, 19, 19, 19, 20, 20, 20, 21, 21, 22, 18, 19, 19, 20, 20, 21, 18, 19, 20, 21, 21, 21]
+
 
 # numpyに変換
-heights_np = np.array(heights, dtype=np.float32)
-
-# 1次元へ変換
-heights_np = heights_np.reshape(-1, 1)
+heights_np = np.array(heights, dtype=np.float32).reshape(-1, 1)
+weights_np = np.array(weights, dtype=np.float32).reshape(-1, 1)
+# 2次元へ変換
+train_np = np.r_[heights_np, weights_np]
 
 # 正常値からの学習
+
 # contamination が全データに含まれる異常値の割合。
 # データの中に10%の異常が含まれる場合、0.1を指定する。
 # 何も指定しないときは、contamination = 0.1
-contamination = 0.1
-clf = IsolationForest(contamination=contamination)
-clf.fit(heights_np)
+clf = IsolationForest(contamination=0)
+clf.fit(train_np)
 print("training finished.")
 
 # ----- ここで正常値からの学習終了 ------
+
 # 学習結果を使って異常値かどうかの判断
 
+# テスト用データ
 # テスト
-# decision_functionの代わりにpredictを使うと、数値ではなく異常かどうかだけを返してくれる
-test_np = np.arange(95, 125, 1).reshape(-1, 1)
-results = clf.decision_function(test_np)
+results = clf.predict(train_np)
 
-# 値が小さいほど異常値とみなせる
-b1 = plt.scatter(test_np, results, c='white', s=20, edgecolor='k')
-filename = "plot_" + str(contamination) + '.png'
-plt.savefig(filename)
+xx, yy = np.meshgrid(np.linspace(90, 120, 50), np.linspace(10, 30, 50))
+Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+plt.title("IsolationForest")
+plt.contourf(xx, yy, Z, cmap=plt.cm.Blues_r)
 plt.show()
-
+print(results)
 
 
 
