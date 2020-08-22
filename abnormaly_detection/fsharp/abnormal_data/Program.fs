088bdf7e5fd0e639c6088dcfdaefaaf2c68e2499
natsutan <YRW03704@nifty.com>
2020-08-23 08:11:52 +0900

    異常検知終了

---
+++ abnormaly_detection/fsharp/abnormal_data/Program.fs
@@ -0,0 +1,94 @@
+﻿// Learn more about F# at https://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open MathNet.Numerics.Distributions
+open FSharp.Charting
+
+type GaussDistri = { mu :float ; sigma :float}
+let randgen = System.Random()
+let output_path = @"C:\home\myproj\study_ml\abnormaly_detection\fsharp\abnormal_data\output\"
+
+
+let sampleData(gd0:GaussDistri, gd1:GaussDistri, pi:float):float = 
+    
+    if randgen.NextDouble() < pi then
+        Normal(mean=gd0.mu, stddev= gd0.sigma).Sample()
+    else
+        Normal(mean=gd1.mu, stddev= gd1.sigma).Sample()
+
+
+let generateData = 
+    let N = 1000
+    let pi0 = 0.6
+    let gd0:GaussDistri = {mu = 3.0; sigma =  0.5}
+    let gd1:GaussDistri = {mu = 0.0; sigma =  3.0}
+
+    [| for _ in 1 .. N -> sampleData(gd0, gd1, pi0) |]
+
+
+let plotData (data:double[]) = 
+    let n = Seq.length data
+
+    let chart =  Chart.Point [for i in 0 .. n - 1 -> (i + 1,  data.[i]) ]
+    let output_file = output_path + @"em.png" 
+
+    Chart.Save output_file chart
+    printf "%s\n"  output_file
+    None
+
+let calcMu(x:double[], q:double[], pi:double[]):float =
+    let numerator = Array.map2 (fun x y -> x * y) x q |> Array.sum
+    let denominator = Array.sum q
+
+    numerator / denominator
+
+let calcSigma(x:double[], q:double[], mu:float, pi:float) :float = 
+    let x_minus_mu = Array.map (fun x -> (x - mu)**2.0 ) x
+    let numerator = Array.map2 (fun x y -> x * y) q x_minus_mu|> Array.sum
+    
+    let denominator = Array.sum q
+
+    sqrt (numerator / denominator)
+
+
+let em (data:double[]) = 
+    //初期値
+    let mutable pi0 = 0.5
+    let mutable pi1 = 0.5
+    let mutable mu0 = 5.0
+    let mutable mu1 = -5.0
+    let mutable sigma0 = 1.0
+    let mutable sigma1 = 5.0
+
+    let mutable qn0 = 0.0
+    let mutable qn1 = 0.0
+
+    for _ in 0 ..10 do
+        let piN0 = Array.map (fun x -> pi0 * Normal.PDF(mu0, sigma0, x)) data
+        let piN1 = Array.map (fun x -> pi1 * Normal.PDF(mu1, sigma1, x)) data
+        let qn0 = Array.map2 (fun p0 p1 -> p0 / (p0 + p1)) piN0 piN1
+        let qn1 = Array.map2 (fun p0 p1 -> p1 / (p0 + p1)) piN0 piN1
+        pi0 <- Array.average qn0
+        pi1 <- Array.average qn1
+        mu0 <- calcMu(data, qn0, piN0)
+        mu1 <- calcMu(data, qn1, piN1)
+        sigma0 <- calcSigma(data, qn0, mu0, pi0)
+        sigma1 <- calcSigma(data, qn1, mu1, pi1)
+
+    let gd0:GaussDistri = {mu = mu0; sigma =  sigma0}
+    let gd1:GaussDistri = {mu = mu1; sigma =  sigma1}
+
+    gd0, gd1, (pi0, pi1)
+    
+
+[<EntryPoint>]
+let main argv =
+    let data = generateData
+    plotData data |> ignore
+    let gd0, gd1, pis = em data
+
+    printf "gd0 %A\n" gd0
+    printf "gd1 %A\n" gd1
+    printf "%A\n" pis
+
+
+    0 // return an integer exit code
---
+++ abnormaly_detection/fsharp/eval/Program.fs
@@ -0,0 +1,69 @@
+﻿// Learn more about F# at https://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open FSharp.Charting
+
+type Sample = {score:float; anomality:bool}
+let output_path = @"C:\home\myproj\study_ml\abnormaly_detection\fsharp\eval\output\"
+
+let makeData :List<Sample> = 
+//    let mutable data = []
+    let scores = [0.19; 0.86; 0.17; 0.12; 0.04; 0.78; 0.16; 0.51; 0.57; 0.27]
+    let anomalies = [false; true;false;false;false;true;false;true;false;false]
+
+    List.map2 (fun s a -> {score=s; anomality = a}) scores anomalies
+
+let calcRecallDetection(data:List<Sample>) = 
+    let N = List.length data
+    let recall:float[] = Array.create N 0.0
+    let detection:float[] = Array.create N 0.0
+
+    let anom_cnt = List.filter (fun x -> x.anomality) data |> List.length
+    let norm_cnt = N - anom_cnt
+
+    for i in 0 .. 9 do
+        let d = data.[0..i]
+        let danom = List.filter (fun x -> x.anomality) d |> List.length
+        let dnorm = norm_cnt - (i+1 - danom)
+
+        Array.set recall i (float(danom) / float(anom_cnt))
+        Array.set detection  i (float(dnorm) / float(norm_cnt))
+
+
+    (recall,detection)
+
+
+let plot(data:List<Sample>, recall:float[], detection:float[]) = 
+
+    let mutable recall_plot = [(1.0, 0.0)]
+    let mutable detection_plot = [(1.0, 1.0)]
+    let n = List.length data
+    
+    for i in 0 .. n - 1 do
+        let score = data.[i].score
+        recall_plot <- (score, recall.[i]) :: recall_plot
+        detection_plot <- (score, detection.[i]) :: detection_plot
+            
+    let chart1 = Chart.Line(recall_plot)
+    let chart2 = Chart.Line(detection_plot)
+    
+    let chart_comb = Chart.Combine([chart1; chart2]).WithYAxis(Max = 1.0, Min = 0.0)
+    let output_file = output_path + @"fvalue.png" 
+    Chart.Save output_file chart_comb
+
+    printf "%s\n"  output_file
+
+
+
+[<EntryPoint>]
+let main argv =
+    let data = makeData
+    let sorted_data = List.sortBy (fun elem -> elem.score) data |> List.rev
+
+    let recall, detection = calcRecallDetection sorted_data
+    
+    plot (sorted_data, recall, detection) |> ignore
+
+    printfn "%A" recall
+    printfn "%A" detection
+
+    0 // return an integer exit code
